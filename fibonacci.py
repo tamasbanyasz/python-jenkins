41ad4dffd0b1c325e3e1e908c774baefa2f8550f
Bányász Tamás <tamasbanyasz@gmail.com>
2024-10-19 21:48:32 +0200

    Files added

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    sequence = [0, 1]
+    for i in range(2, n):
+        sequence.append(sequence[-1] + sequence[-2])
+    
+    return sequence
